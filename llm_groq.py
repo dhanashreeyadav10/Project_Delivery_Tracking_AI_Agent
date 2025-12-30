5dfcb0ca2972a3ff3a3dc4c95561177e0e977170
Dhanashree Yadav <120458692+dhanashreeyadav10@users.noreply.github.com>
2025-12-30 15:23:38 +0530

    llm_groq.py

--- llm_groq.py
+++ llm_groq.py
@@ -11,15 +11,16 @@ def get_llm():
     return Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 
-# def explain_insight(prompt: str) -> str:
-#     try:
-#         client = Groq(api_key=os.getenv("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"))
-#         response = client.chat.completions.create(
-#             model="llama-3.1-8b-instant",
-#             messages=[{"role": "user", "content": prompt}],
-#             timeout=10
-#         )
-#         return response.choices[0].message.content
-#     except Exception as e:
-#         return f"LLM unavailable: {e}"
+def explain_insight(prompt: str) -> str:
+    try:
+        client = Groq(api_key=os.getenv("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"))
+        response = client.chat.completions.create(
+            model="llama-3.1-8b-instant",
+            messages=[{"role": "user", "content": prompt}],
+            timeout=10
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        return f"LLM unavailable: {e}"
+
 
