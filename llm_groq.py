ed05097215c8d5437d01378983ff204f65b392db
Dhanashree Yadav <120458692+dhanashreeyadav10@users.noreply.github.com>
2025-12-30 15:23:08 +0530

    llm_groq.py

--- llm_groq.py
+++ llm_groq.py
@@ -4,14 +4,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def explain_insight(prompt: str) -> str:
-    try:
-        client = Groq(api_key=os.getenv("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"))
-        response = client.chat.completions.create(
-            model="llama-3.1-8b-instant",
-            messages=[{"role": "user", "content": prompt}],
-            timeout=10
-        )
-        return response.choices[0].message.content
-    except Exception as e:
-        return f"LLM unavailable: {e}"
+from groq import Groq
+import os
+
+def get_llm():
+    return Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+
+# def explain_insight(prompt: str) -> str:
+#     try:
+#         client = Groq(api_key=os.getenv("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"))
+#         response = client.chat.completions.create(
+#             model="llama-3.1-8b-instant",
+#             messages=[{"role": "user", "content": prompt}],
+#             timeout=10
+#         )
+#         return response.choices[0].message.content
+#     except Exception as e:
+#         return f"LLM unavailable: {e}"
+
