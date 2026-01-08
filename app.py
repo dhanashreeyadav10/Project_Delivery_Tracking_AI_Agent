5c25b869d65dbc1d32689389b13e56fc483dc5a1
Dhanashree Yadav <120458692+dhanashreeyadav10@users.noreply.github.com>
2026-01-08 18:12:03 +0530

    app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.set_page_config(
 # -------------------------------
 # HEADER
 # -------------------------------
-logo = Image.open("assets/compunnel_logo.png")
+logo = Image.open("compunnel_logo.jpg")
 c1, c2 = st.columns([1, 6])
 
 with c1:
@@ -147,3 +147,4 @@ if st.button("🧠 Get Answer"):
             question, util_df, risk_df, cost_df, hr_df
         )
     st.success(response)
+
