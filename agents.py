073265a0460f521cfddeb4ed1d9f6d8b9cc357c0
Dhanashree Yadav <120458692+dhanashreeyadav10@users.noreply.github.com>
2026-01-08 18:10:46 +0530

    agents.py

--- agents.py
+++ agents.py
@@ -1,18 +1,18 @@
 class UtilizationAgent:
-    def run(self, util_df):
-        return util_df[util_df["utilization_pct"] < 60]
+    def run(self, df):
+        return df[df["utilization_pct"] < 60]
 
 
 class DeliveryRiskAgent:
-    def run(self, risk_df):
-        return risk_df[risk_df["risk_flag"] == 1]
+    def run(self, df):
+        return df[df["risk_flag"] == 1]
 
 
 class CostMarginAgent:
-    def run(self, cost_df):
-        return cost_df[cost_df["margin"] < 0]
+    def run(self, df):
+        return df[df["margin"] < 0]
 
 
 class HRRiskAgent:
-    def run(self, hr_df):
-        return hr_df[hr_df["hr_risk"] == 1]
+    def run(self, df):
+        return df[df["hr_risk"] == 1]
